5ffd1215eb52e25b3d6eaf3a9fda23c0ac420faf
Bartłomiej Jamiołkowski <61807667+MindBreaker20@users.noreply.github.com>
2022-05-09 23:36:52 +0200

    Update exponantially_weighted_moving_average_crossover.py

--- Trend Following/exponantially_weighted_moving_average_crossover.py
+++ Trend Following/exponantially_weighted_moving_average_crossover.py
@@ -3,3 +3,6 @@ from yahoo_fin.stock_info import get_data
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Apple Inc. market data
+appl_data = get_data("aapl", start_date="01/01/2014", end_date="31/12/2019", index_as_date = True, interval="1d")

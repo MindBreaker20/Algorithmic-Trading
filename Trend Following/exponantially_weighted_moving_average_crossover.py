392a2c7adf9ce04d66e2bbde53dca521a8b3cacd
Bartłomiej Jamiołkowski <61807667+MindBreaker20@users.noreply.github.com>
2022-05-09 23:51:59 +0200

    Update exponantially_weighted_moving_average_crossover.py

--- Trend Following/exponantially_weighted_moving_average_crossover.py
+++ Trend Following/exponantially_weighted_moving_average_crossover.py
@@ -17,4 +17,24 @@ def ewma(data_set, n): # there are two arguments data frame column, which repres
         ewm_i = alpha * data_set[i + n] + (1 - alpha) * window[i] # EWMA equation
         window.append(ewm_i) # adding calculated EWMA to the list
     return window # returning the list of calculated EWMA
-  
+
+# Calculating short and long EWMA (20 and 50 days)
+ewma_daily_20 = ewma(appl_data['close'], 20) 
+ewma_daily_50 = ewma(appl_data['close'], 50)
+
+# Creating new data frame
+appl_data['ewma_20'] = np.nan # empty columns need to be created as there is need to present short and long EWMa on the timeseries
+appl_data['ewma_50'] = np.nan
+
+# Populating new columns with EWMA values from lists
+for i in range(0, len(appl_data['close']) - 20):
+    appl_data['ewma_20'][i + 20] = ewma_daily_20[i]
+
+for i in range(0, len(appl_data['close']) - 50):
+    appl_data['ewma_50'][i + 50] = ewma_daily_50[i]
+    
+# Calculating trade signals (0 - sell asset, 1 - buy asset)
+
+
+
+# Visualization
